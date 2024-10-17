1b8a97ff6043e3b146d343e1f1349f4cfd30c5d1
Utkarsh Singh <utkarshsingh30112002@gmail.com>
2024-10-17 10:59:10 +0530

    reduced event listners

--- counter/index.js
+++ counter/index.js
@@ -1,22 +1,21 @@
-const increment=document.querySelector('#increment');
-const decrement=document.querySelector('#decrement');
 const counter=document.querySelector('#counter');
-const reset=document.querySelector('#reset');
 const variable=document.querySelector("#increment-variable");
 
-increment.addEventListener('click',()=>{
-    let incrementor=  parseInt(variable.value);
-    if(!incrementor)return
-    incrementor+=parseInt(counter.innerText)
-    counter.innerHTML=incrementor;
-})
-decrement.addEventListener('click',()=>{
-    let decrementor=parseInt(variable.value);
-    if(!decrementor)return
-    decrementor=parseInt(counter.innerText)-decrementor;
-    counter.innerText=decrementor;
-})
-reset.addEventListener('click',()=>{
-    variable.value=0;
-    counter.innerHTML=0;
+document.body.addEventListener('click',(e)=>{
+    if(e.target.id=='increment'){
+        let incrementor=  parseInt(variable.value);
+        if(!incrementor)return
+        incrementor+=parseInt(counter.innerText)
+        counter.innerHTML=incrementor;
+    }
+    else if(e.target.id=='decrement'){
+        let decrementor=parseInt(variable.value);
+        if(!decrementor)return
+        decrementor=parseInt(counter.innerText)-decrementor;
+        counter.innerText=decrementor;
+    }
+    else if(e.target.id=='reset'){
+        variable.value=0;
+        counter.innerHTML=0;
+    }
 })
--- transfer-list/index.js
+++ transfer-list/index.js
@@ -1,136 +1,120 @@
-const allLeft=document.querySelector('#all-left');
-const allRight=document.querySelector('#all-right');
-const selectedLeft=document.querySelector('#selected-left');
-const selectedRight=document.querySelector('#selected-right');
+const allLeft = document.querySelector("#all-left");
+const allRight = document.querySelector("#all-right");
+const selectedLeft = document.querySelector("#selected-left");
+const selectedRight = document.querySelector("#selected-right");
 
-const right=document.querySelector('.right')
-const left=document.querySelector('.left')
-const between=document.querySelector('.between')
+const right = document.querySelector(".right");
+const left = document.querySelector(".left");
+const between = document.querySelector(".between");
 
-let l=[]
-let r=[]
+let l = [];
+let r = [];
 
-const all=document.querySelectorAll('input');
-all.forEach(checkbox=>{
-    checkbox.addEventListener('change',()=>{
-        if(checkbox.checked){
-            if(checkbox.parentElement.parentElement.classList.contains('left')){
-                selectedRight.style.pointerEvents='auto';
-                selectedRight.parentElement.style.cursor='pointer';
-                l.push(checkbox.parentElement);
-            }
-            else{
-                selectedLeft.style.pointerEvents='auto';
-                selectedLeft.parentElement.style.cursor='pointer';
-                r.push(checkbox.parentElement);
-            }
-            console.log(l)
-            console.log(r)
-        }
-        else{
-            if(checkbox.parentElement.parentElement.classList.contains('left')){
-                l=l.filter(item=>item!=checkbox.parentElement);
-                if(l.length==0){
-                    selectedRight.style.pointerEvents='none'
-                    selectedRight.parentElement.style.cursor='not-allowed'
-                }
-            }
-            else{
-                r=r.filter(item=>item!=checkbox.parentElement);
-                if(r.length==0){
-                    selectedLeft.style.pointerEvents='none'
-                    selectedLeft.parentElement.style.cursor='not-allowed'
-                }
-            }
-            console.log(l)
-            console.log(r)
-        }
-    })
-})
-
-allRight.addEventListener('click',()=>{
-    const children=left.children
-    while(children.length!=0){
-        right.appendChild(children[0])
-    }
-    r.push(...l);
-    l=[]
-
-    selectedRight.style.pointerEvents='none'
-    selectedRight.parentElement.style.cursor='not-allowed'
-    allRight.style.pointerEvents='none'
-    allRight.parentElement.style.cursor='not-allowed'
-    if(r.length>0){
-    selectedLeft.style.pointerEvents='auto'
-    selectedLeft.parentElement.style.cursor='pointer'
-    }
-    else{
-    selectedLeft.style.pointerEvents='none'
-    selectedLeft.parentElement.style.cursor='not-allowed'}
-    allLeft.style.pointerEvents='auto'
-    allLeft.parentElement.style.cursor='pointer'
-
-})
-allLeft.addEventListener('click',()=>{
-    const children=right.children
-    while(children.length!=0){
-        left.appendChild(children[0])
+document.querySelector(".container").addEventListener("click", (e) => {
+    console.log(e.target)
+  if (e.target.id == "all-left") {
+    const children = right.children;
+    while (children.length != 0) {
+      left.appendChild(children[0]);
     }
     l.push(...r);
-    r=[]
-    selectedLeft.style.pointerEvents='none'
-    selectedLeft.parentElement.style.cursor='not-allowed'
-    allLeft.style.pointerEvents='none'
-    allLeft.parentElement.style.cursor='not-allowed'
-    if(l.length==0){
-        selectedRight.style.pointerEvents='none'
-        selectedRight.parentElement.style.cursor='not-allowed'
+    r = [];
+    selectedLeft.style.pointerEvents = "none";
+    selectedLeft.parentElement.style.cursor = "not-allowed";
+    allLeft.style.pointerEvents = "none";
+    allLeft.parentElement.style.cursor = "not-allowed";
+    if (l.length == 0) {
+      selectedRight.style.pointerEvents = "none";
+      selectedRight.parentElement.style.cursor = "not-allowed";
+    } else {
+      selectedRight.style.pointerEvents = "auto";
+      selectedRight.parentElement.style.cursor = "pointer";
     }
-    else{
-    selectedRight.style.pointerEvents='auto'
-    selectedRight.parentElement.style.cursor='pointer'}
-    allRight.style.pointerEvents='auto'
-    allRight.parentElement.style.cursor='pointer'
+    allRight.style.pointerEvents = "auto";
+    allRight.parentElement.style.cursor = "pointer";
+  } else if (e.target.id == "all-right") {
+    const children = left.children;
+    while (children.length != 0) {
+      right.appendChild(children[0]);
+    }
+    r.push(...l);
+    l = [];
 
-})
-selectedRight.addEventListener('click',()=>{
-    const children=left.children
-    let i=0;
-    while(i<children.length){
-        if(children[i].firstElementChild.checked){
-            children[i].firstElementChild.checked=false
-            right.appendChild(children[i])
+    selectedRight.style.pointerEvents = "none";
+    selectedRight.parentElement.style.cursor = "not-allowed";
+    allRight.style.pointerEvents = "none";
+    allRight.parentElement.style.cursor = "not-allowed";
+    if (r.length > 0) {
+      selectedLeft.style.pointerEvents = "auto";
+      selectedLeft.parentElement.style.cursor = "pointer";
+    } else {
+      selectedLeft.style.pointerEvents = "none";
+      selectedLeft.parentElement.style.cursor = "not-allowed";
+    }
+    allLeft.style.pointerEvents = "auto";
+    allLeft.parentElement.style.cursor = "pointer";
+  } else if (e.target.id === "selected-right") {
+    const children = left.children;
+    let i = 0;
+    while (i < children.length) {
+      if (children[i].firstElementChild.checked) {
+        children[i].firstElementChild.checked = false;
+        right.appendChild(children[i]);
+      } else i++;
+    }
+    l = [];
+    if (children.length == 0) {
+      allRight.style.pointerEvents = "none";
+      allRight.parentElement.style.cursor = "not-allowed";
+    }
+    selectedRight.style.pointerEvents = "none";
+    selectedRight.parentElement.style.cursor = "not-allowed";
+    allLeft.style.pointerEvents = "auto";
+    allLeft.parentElement.style.cursor = "pointer";
+  } else if (e.target.id === "selected-left") {
+    const children = right.children;
+    let i = 0;
+    while (i < children.length) {
+      if (children[i].firstElementChild.checked) {
+        children[i].firstElementChild.checked = false;
+        left.appendChild(children[i]);
+      } else i++;
+    }
+    r = [];
+    if (children.length == 0) {
+      allLeft.style.pointerEvents = "none";
+      allLeft.parentElement.style.cursor = "not-allowed";
+    }
+    selectedLeft.style.pointerEvents = "none";
+    selectedLeft.parentElement.style.cursor = "not-allowed";
+    allRight.style.pointerEvents = "auto";
+    allRight.parentElement.style.cursor = "pointer";
+  } else if (e.target.tagName == "INPUT" && e.target.type == "checkbox") {
+    let checkbox = e.target;
+    if (checkbox.checked) {
+      if (checkbox.parentElement.parentElement.classList.contains("left")) {
+        selectedRight.style.pointerEvents = "auto";
+        selectedRight.parentElement.style.cursor = "pointer";
+        l.push(checkbox.parentElement);
+      } else {
+        selectedLeft.style.pointerEvents = "auto";
+        selectedLeft.parentElement.style.cursor = "pointer";
+        r.push(checkbox.parentElement);
+      }
+    } else {
+      if (checkbox.parentElement.parentElement.classList.contains("left")) {
+        l = l.filter((item) => item != checkbox.parentElement);
+        if (l.length == 0) {
+          selectedRight.style.pointerEvents = "none";
+          selectedRight.parentElement.style.cursor = "not-allowed";
         }
-        else i++;
-    }
-    l=[]
-    if(children.length==0){
-    allRight.style.pointerEvents='none'
-    allRight.parentElement.style.cursor='not-allowed'
-    }
-    selectedRight.style.pointerEvents='none'
-    selectedRight.parentElement.style.cursor='not-allowed'
-    allLeft.style.pointerEvents='auto'
-    allLeft.parentElement.style.cursor='pointer'
-})
-selectedLeft.addEventListener('click',()=>{
-    const children=right.children
-    let i=0;
-    while(i<children.length){
-        if(children[i].firstElementChild.checked){
-            children[i].firstElementChild.checked=false
-            left.appendChild(children[i])
+      } else {
+        r = r.filter((item) => item != checkbox.parentElement);
+        if (r.length == 0) {
+          selectedLeft.style.pointerEvents = "none";
+          selectedLeft.parentElement.style.cursor = "not-allowed";
         }
-        else i++;
+      }
     }
-    r=[]
-    if(children.length==0){
-        allLeft.style.pointerEvents='none'
-        allLeft.parentElement.style.cursor='not-allowed'
-    }
-    selectedLeft.style.pointerEvents='none'
-    selectedLeft.parentElement.style.cursor='not-allowed'
-    allRight.style.pointerEvents='auto'
-    allRight.parentElement.style.cursor='pointer'
-})
-
+  }
+});
